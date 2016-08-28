7220608554e8e23173145afee356a20d136c4138
田村 塁 <qmo@trmbp.local>
2016-08-28 17:58:27 +0900

    CrawlLibrary 1.0.0 公式サイト公開版

---
+++ CrawlLibrary/attitude_sensor.cpp
@@ -0,0 +1,74 @@
+#include <Arduino.h>
+#include<Wire.h>
+#include "attitude_sensor.h"
+
+
+int attitude_data[10];
+
+void initAttitudeSensor() {
+  Wire.beginTransmission(0x68);
+  Wire.write(0x75);
+  Wire.endTransmission();
+  Wire.requestFrom(0x68, 1);
+  
+  delay(10);
+  
+  if (Wire.read() == 0x71) {
+    Serial.println("Success");
+  } else {
+    Serial.println("Failed");
+    while (1);
+  }
+  
+  Wire.beginTransmission(0x68);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  
+  Wire.beginTransmission(0x68);
+  Wire.write(0x37);
+  Wire.write(0x02);
+  Wire.endTransmission();
+  
+  Wire.beginTransmission(0x0C);
+  Wire.write(0x0A);
+  Wire.write(0x12);
+  Wire.endTransmission();
+  
+  for(int i = 0;i < 10;i++){
+     attitude_data[i] = 0;
+  }
+}
+
+void getAttitude() {
+  int calc_temp;
+  byte calc_temp_b;
+  
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B);
+  Wire.endTransmission();
+  Wire.requestFrom(0x68, 14);
+  
+  for (int i = 0; i < 7; i++) {
+    calc_temp = Wire.read();
+    calc_temp = calc_temp << 8;
+    calc_temp |= Wire.read();
+     attitude_data[i] = calc_temp;
+  }
+
+  Wire.beginTransmission(0x0C);
+  Wire.write(0x03);
+  Wire.endTransmission();
+  Wire.requestFrom(0x0C,7);
+  
+  for(int i = 7;i < 10;i++){
+    calc_temp_b = Wire.read();
+    calc_temp = Wire.read();
+    calc_temp = calc_temp << 8;
+    calc_temp |= calc_temp_b;
+     attitude_data[i] = calc_temp;
+  }
+  
+  Wire.read();
+}
+
---
+++ CrawlLibrary/examples/C,C++/CrawlBlank/CrawlBlank.ino
@@ -0,0 +1,18 @@
+/*********************************************
+   Crawl Blank Code
+   C,C++記述プログラムのブランクです
+
+   Written by RuiTamura
+   2016 Omniment Inc.
+*********************************************/
+
+#include <Crawl.h>
+
+int main() {
+  crl.init();
+
+  while (1) {
+
+  }
+}
+
