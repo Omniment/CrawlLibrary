0a789d40c50b5973815b0504d6aa2cdd75ff5acf
gatakaba <m07112@gmail.com>
2016-10-03 23:17:09 +0900

    apply formatter

--- src/util/kalmanfilter.cpp
+++ src/util/kalmanfilter.cpp
@@ -10,7 +10,6 @@ KalmanFilter::KalmanFilter() {
   this->F[1][0] = 0;
   this->F[1][1] = 1;
 
-
   this->transpose((float*)this->F, 2, 2, (float*)this->FT);
 
   this->H[0][0] = 1;
@@ -26,7 +25,6 @@ KalmanFilter::KalmanFilter() {
 
   this->posteriori_covariance[1][0] = 0;
   this->posteriori_covariance[1][1] = 1;
-
 
   this->q1 = 0.0001;
   this->q2 = 0.001;
@@ -66,12 +64,10 @@ void KalmanFilter::multiply(float* A, float* B, int m, int p, int n, float* C) {
   for (i = 0; i < m; i++)
     for (j = 0; j < n; j++) {
       C[n * i + j] = 0;
-      for (k = 0; k < p; k++)
-        C[n * i + j] = C[n * i + j] + A[p * i + k] * B[n * k + j];
+      for (k = 0; k < p; k++) C[n * i + j] = C[n * i + j] + A[p * i + k] * B[n * k + j];
     }
 }
-void KalmanFilter::multiply3(float* A, float* B, float* C, int m, int p, int r,
-                             int n, float* D) {
+void KalmanFilter::multiply3(float* A, float* B, float* C, int m, int p, int r, int n, float* D) {
   // A = input matrix (m x p)
   // B = input matrix (p x r)
   // C = input matrix (r x n)
@@ -103,52 +99,45 @@ void KalmanFilter::inverse2d(float* A, float* B) {
   B[3] = A[0] / det;
 }
 
-void KalmanFilter::update(float theta, float gyro,float gyro_offset) {
+void KalmanFilter::update(float theta, float gyro, float gyro_offset) {
   float observation[2];
   observation[0] = theta;
   observation[1] = gyro - gyro_offset;
 
   // z = F z
-  this->priori_state[0] = this->posteriori_state[0] + this->posteriori_state[1]  * dt;
+  this->priori_state[0] = this->posteriori_state[0] + this->posteriori_state[1] * dt;
   this->priori_state[1] = this->posteriori_state[1];
 
   // P = F P F.T + Q
-  this->multiply3((float*)F, (float*)this->posteriori_covariance,
-                  (float*)this->FT, 2, 2, 2, 2,
+  this->multiply3((float*)F, (float*)this->posteriori_covariance, (float*)this->FT, 2, 2, 2, 2,
                   (float*)this->priori_covariance);
   this->priori_covariance[0][0] += this->q1;
   this->priori_covariance[1][1] += this->q2;
 
   // S = H P H.T + R
-  this->multiply3((float*)H, (float*)this->priori_covariance, (float*)this->HT,
-                  2, 2, 2, 2, (float*)this->S);
+  this->multiply3((float*)H, (float*)this->priori_covariance, (float*)this->HT, 2, 2, 2, 2, (float*)this->S);
   this->S[0][0] += this->r1;
   this->S[1][1] += this->r2;
 
   // K = P H.T S^{-1}
   this->inverse2d((float*)this->S, (float*)this->S_inverse);
-  this->multiply3((float*)this->priori_covariance, (float*)this->HT,
-                  (float*)this->S_inverse, 2, 2, 2, 2, (float*)this->K);
+  this->multiply3((float*)this->priori_covariance, (float*)this->HT, (float*)this->S_inverse, 2, 2, 2, 2,
+                  (float*)this->K);
 
   // e = (x - H z)
   this->estimated_observation[0] = this->priori_state[0];
   this->estimated_observation[1] = this->priori_state[1];
-  this->subtract((float*)observation, (float*)this->estimated_observation, 2, 1,
-                 (float*)this->e);
+  this->subtract((float*)observation, (float*)this->estimated_observation, 2, 1, (float*)this->e);
 
   // z = z + K e
-  this->multiply((float*)this->K, (float*)this->e, 2, 2, 1,
-                 (float*)this->state_modify);
-  this->add((float*)this->priori_state, (float*)this->state_modify, 2, 1,
-            (float*)this->posteriori_state);
+  this->multiply((float*)this->K, (float*)this->e, 2, 2, 1, (float*)this->state_modify);
+  this->add((float*)this->priori_state, (float*)this->state_modify, 2, 1, (float*)this->posteriori_state);
 
   // P = P - K  S  K.T
   this->transpose((float*)this->K, 2, 2, (float*)this->KT);
-  this->multiply3((float*)this->K, (float*)this->S, (float*)this->KT, 2, 2, 2,
-                  2, (float*)this->covariance_modify);
+  this->multiply3((float*)this->K, (float*)this->S, (float*)this->KT, 2, 2, 2, 2, (float*)this->covariance_modify);
 
-  this->subtract((float*)this->priori_covariance,
-                 (float*)this->covariance_modify, 2, 2,
+  this->subtract((float*)this->priori_covariance, (float*)this->covariance_modify, 2, 2,
                  (float*)this->posteriori_covariance);
 }
 
