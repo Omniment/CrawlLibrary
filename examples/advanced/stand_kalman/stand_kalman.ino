1ebb25c308d2ad73a6fca6bf673f940ee6276f1c
gatakaba <m07112@gmail.com>
2016-10-09 17:19:02 +0900

    update stand_kalman

--- examples/advanced/stand_kalman/stand_kalman.ino
+++ examples/advanced/stand_kalman/stand_kalman.ino
@@ -2,9 +2,9 @@
 #define CRL_PI 3.14159265358979  //円周率を定義
 
 int main() {
-  float Kp1 = 12.0;              // 角度制御比例ゲイン (調節パラメータ)
-  float Kp2 = 3.0;               // 上端速度制御比例ゲイン (調節パラメータ)
-  float theta_d_offset = 0.036;  // 重心の偏り(調節パラメータ)
+  float kp1 = 5.0;   // 角度制御比例ゲイン (調節パラメータ)
+  float kp2 = 8.0;   // 上端速度制御比例ゲイン (調節パラメータ)
+  float ki2 = 40.0;  // 上端速度制御積分ゲイン (調節パラメータ)
 
   float dt = 0.010;              // サンプリング時間 [s]
   float theta;                   // 角度 [rad]
@@ -15,18 +15,33 @@ int main() {
   float err2;                    // 目標上端速度と実上端速度の偏差
   float u;                       // 制御入力  -1.0〜0〜1.0
 
+  Integral err2i;
+  err2i.setDt(dt);
+  err2i.setLimit(-5.0, 5.0);
+
+  FirstOrderFilter fof_err2;
+  fof_err2.setDt(dt);
+  fof_err2.setT(1.0 / 15);
+
+  FirstOrderFilter fof_err2i;
+  fof_err2i.setDt(dt);
+  fof_err2i.setT(1.0 / 5);
+
   crl.init();     // ロボットの初期化
   crl.setDt(dt);  // サンプリング時間を設定
 
   while (1) {
     crl.realtimeLoop();                     // dt[s]ごとに以下ループを実行
     crl.updateState();                      // 各種センサ情報取得,モータ出力の更新
-    theta = crl.getThetaZ();                // クロールの実姿勢角度を取得
+    theta = crl.getThetaKalman();                // クロールの実姿勢角度を取得
     head_velocity = crl.getHeadVelocity();  // クロールの実上端速度を取得
 
-    err1 = (theta_d + theta_d_offset) - theta;  // 目標角度と実角度の偏差を計算
-    err2 = head_velocity_d - head_velocity;     // 目標上端速度と実上端速度の偏差を計算
-    u = err1 * Kp1 + err2 * Kp2;                // P制御により制御入力を計算
+    err1 = (theta_d + fof_err2i.getOutput() * ki2) - theta;  // 目標角度と実角度の偏差を計算
+    err2 = head_velocity_d - head_velocity;                  // 目標上端速度と実上端速度の偏差を計算
+    fof_err2.calculate(err2);
+    err2i.calculate(err2);
+    fof_err2i.calculate(err2i.getOutput());
+    u = err1 * kp1 + fof_err2.getOutput() * kp2;  // P制御により制御入力を計算
 
     if (theta < CRL_PI * 1.0 / 4.0 || CRL_PI * 3.0 / 4.0 < theta) {  // クロールの姿勢θがPI/2付近以外でモータを停止
       u = 0;
